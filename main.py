6880e47a84ac159c213a34e7f13ec1af0fc37fd7
hashamzaki <hashamzaki123@gmail.com>
2023-03-02 18:32:50 +0500

    first commit in master

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def sum(n1,n2):
+    # Use a breakpoint in the code line below to debug your script.
+    return n1+n2  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(sum(2,3))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
